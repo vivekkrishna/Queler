79f317c5ea733e0c5b3d1faf08bd285ceba48141
Vivek Krishna <choppak@amazon.com>
2019-07-16 08:52:51 +0530

    Picking configurations from beanstalk environment variables.

--- library/config.php
+++ library/config.php
@@ -1,9 +1,9 @@
 <?php //Database connection settings
-defined('DBH') ? null : define ('DBH' , 'aa1ipe9l9hafvx3.c8xa5yyvumqn.us-west-2.rds.amazonaws.com');
-defined('DBU') ? null : define ('DBU' , 'choppak');
-defined('DBPW') ? null : define ('DBPW' , '10011993');
-defined('DBN') ? null : define ('DBN' , 'ebdb');
-defined('DBTP') ? null : define ('DBTP' , 'Queler_Prod_');
+defined('DBH') ? null : define ('DBH' , $_SERVER['RDS_HOSTNAME']);
+defined('DBU') ? null : define ('DBU' , $_SERVER['RDS_USERNAME']);
+defined('DBPW') ? null : define ('DBPW' , $_SERVER['RDS_PASSWORD']);
+defined('DBN') ? null : define ('DBN' , $_SERVER['RDS_DB_NAME']);
+defined('DBTP') ? null : define ('DBTP' , $_SERVER['QuelerDBTablesPrefix']);
 
 //Define your web accessible link to this script, including http:// or https:// with TRAILING SLASH / in the end !IMPORTANT
 defined('WEB_LINK') ? null : define('WEB_LINK' , 'http://www.queler-env.b7zekbinek.us-west-2.elasticbeanstalk.com/');
@@ -11,19 +11,19 @@ defined('ERROR_LINK') ? null : define('ERROR_LINK' , WEB_LINK );
 defined('UPL_FILES') ? null : define('UPL_FILES' , WEB_LINK.'public');
 
 //Facebook API Credentials, get them from https://developers.facebook.com/apps
-$facebook_api = array("secret"=>"", "id" => "");
+$facebook_api = array("secret" => $_SERVER['QuelerFacebookSecret'], "id" => $_SERVER['QuelerFacebookClientID']);
 
 //Google API Credentials, get them from https://console.developers.google.com
-$google_api = array("secret"=>"", "id" => "");
+$google_api = array("secret" => $_SERVER['QuelerGoogleWebSigninClientSecret'], "id" => $_SERVER['QuelerGoogleWebSigninClientID']);
 
 //Google Captcha Info, get them from https://www.google.com/recaptcha/admin
-$captcha_info = array("secret"=>"6LfE7KwUAAAAAE9FhtmSn7L3vQu208Hl0kHtuKCE", "sitekey" => "6LfE7KwUAAAAAFTv83SWOY_l0mbglLsDJx4DBe1o");
+$captcha_info = array("secret" => $_SERVER['QuelerGoogleRecaptchaSecretKey'], "sitekey" => $_SERVER['QuelerGoogleRecaptchaSiteKey']);
 
 //Google Analytics Info, get them from https://analytics.google.com/analytics/web/
-$analytics_info = Array("UA" => "AnalyticsID" );
+$analytics_info = Array("UA" => $_SERVER['QuelerGoogleAnalyticsID'] );
 
 //AddThis Info, get them from https://www.addthis.com/dashboard/
-$addthis_info = Array("ra" => "AddThisID" );
+$addthis_info = Array("ra" => $_SERVER['QuelerAddThisID'] );
 
 $current = dirname(__FILE__);
 
